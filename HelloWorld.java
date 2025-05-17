2f175d0de44411d29b4351bad8a5480ca0bd1d8e
kalyanvajrapu8 <46592983+kalyanvajrapu8@users.noreply.github.com>
2025-05-17 11:26:00 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+class HelloWorld 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Welcome to Leena IT"); 
+		System.out.println("Thanks for joining the class");
+		System.out.println("We are learning about DevOps");
+    } 
+} 
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# HelloWorld
+# Hello-World
+For DEV OPS Practice
+First step - Dev Ops Basics
+Second step - GIT commands
+
+Peforming Commit step
+Next Operation - Pull 
+Comparasion is not happening...
